e1d2ff76e575a7cbd17fced9a531e408892025e7
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-05 15:54:50 +0000

    feat: aggiunta del ModelController con un endpoint

---
+++ hello-world/src/main/java/it/marconi/hello_world/controller/ModelController.java
@@ -0,0 +1,17 @@
+package it.marconi.hello_world.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/other")
+public class ModelController {
+
+    @GetMapping("/fixed")
+    public ModelAndView handlerFixed(){
+        return new ModelAndView("hello-world-model").addObject("maker", "A. Trentini");
+    }
+    
+}
