aa0619ae4a5b10fb680b14a9d35864151c75c1c1
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-09 13:57:47 +0000

    feat: aggiunta di handler per query string e path nel ModelController

--- hello-world/src/main/java/it/marconi/hello_world/controller/ModelController.java
+++ hello-world/src/main/java/it/marconi/hello_world/controller/ModelController.java
@@ -2,7 +2,9 @@ package it.marconi.hello_world.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +15,14 @@ public class ModelController {
     public ModelAndView handlerFixed(){
         return new ModelAndView("hello-world-model").addObject("maker", "A. Trentini");
     }
+
+    @GetMapping("query")
+    public ModelAndView handlerQuery(@RequestParam(defaultValue = "none") String param) {
+        return new ModelAndView("hello-world-model").addObject("maker", param);
+    }
     
+    @GetMapping("path/{username}")
+    public ModelAndView handlerPath(@PathVariable String username) {
+        return new ModelAndView("hello-world-model").addObject("maker", username);
+    }
 }
