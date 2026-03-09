655ee7f43513443f2e2947673565901476af64fe
Daniel-Mittem <daniel.mittempergher@marconirovereto.it>
2026-03-09 14:25:29 +0000

    feat: aggiunta di handler per header e body nel ModelController e creazione di template HTML per le risposte

--- hello-world/src/main/java/it/marconi/hello_world/controller/ModelController.java
+++ hello-world/src/main/java/it/marconi/hello_world/controller/ModelController.java
@@ -3,6 +3,9 @@ package it.marconi.hello_world.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,5 +27,17 @@ public class ModelController {
     @GetMapping("path/{username}")
     public ModelAndView handlerPath(@PathVariable String username) {
         return new ModelAndView("hello-world-model").addObject("maker", username);
+    }
+
+    // --------------  Per le seguenti richieste non è possibile usare un browser, serve un software di terze parti -------------------------------------------
+
+    @GetMapping(path = "header", headers = "myHeader")
+    public ModelAndView handlerHeader(@RequestHeader(defaultValue = "none") String myHeader) {
+        return new ModelAndView("hello-world-header").addObject("header", myHeader);
+    }
+
+    @PostMapping("body")
+    public ModelAndView handlerBody(@RequestBody String body) {
+        return new ModelAndView("hello-world-body");
     }
 }
